ff143489e040add761e7e2778d96aa2f68711284
RyoKiritani1309 <nguyenhungtan1309@gmail.com>
2025-11-15 16:22:44 +0700

    fixed

---
+++ src/dto/order/order.ts
@@ -0,0 +1,41 @@
+import { ApiPaginationResponseDto } from "@dtos/api/api.response.dto";
+
+export type OrderStatus = 'Pending' | 'Shipping' | 'Completed' | 'Cancelled';
+
+export interface SellerOrderItemDto {
+  productName: string;
+  imageUrl: string;
+  variantInfo: string;
+  quantity: number;
+  unitPrice: number;
+}
+export interface SellerOrderDto {
+  orderId: number;
+  customerName: string;
+  phone: string;
+  shippingAddress: string;
+  orderDate: string;
+  status: OrderStatus;
+  totalAmountForSeller: number;
+  items: SellerOrderItemDto[];
+}
+
+export interface BackendOrderItemDto {
+  productName: string;
+  imageUrl: string;
+  variantName: string;
+  quantity: number;
+  price: number;
+}
+export interface BackendOrderDto {
+  id: number;
+  shippingName: string;
+  shippingPhone: string;
+  shippingAddress: string;
+  createdAt: string;
+  status: OrderStatus;
+  totalAmount: number;
+  items: BackendOrderItemDto[];
+}
+
+export type SellerOrderApiResponse = ApiPaginationResponseDto<BackendOrderDto>;
